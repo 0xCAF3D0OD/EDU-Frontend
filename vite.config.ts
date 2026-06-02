dfc3f844778ec05d92b0ab99494b5a98db9363d2
kdi-noce <kevin.dinocera@protonmail.com>
2026-06-02 21:20:33 +0200

    fix: change for get the same result as react but with vuejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    allowedHosts: ['xn--alloremplaant-rgb.ch'] // <-- Ajoute cette ligne
-  }
+    plugins: [vue(), tailwindcss()],
+    server: {
+        allowedHosts: ['xn--alloremplaant-rgb.ch']
+    }
 })
